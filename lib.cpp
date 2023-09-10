38e9d32d207389e6c8424357334094e46f03ffe0
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-10 19:20:13 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool Ricerca(char elenconomi[10][20], char nome[],  int &posizione) {
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 20; ++j) {
+            if(elenconomi[i][j]!=nome[j]){
+                j=20;
+            } else {
+                if(j==19){
+                    posizione=i;
+                    return true;
+                }
+            }
+
+        }
+    }
+    return false;
+}
