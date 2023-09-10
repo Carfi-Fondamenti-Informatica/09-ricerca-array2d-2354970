c0e7664975156de04d8dd3533b1327821d592f38
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-10 19:20:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char elenconomi[10][20];
+    char nome[20];
+    int posizione=0;
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 20; ++j) {
+            nome[j]=' ';
+            elenconomi[i][j]=' ';
+        }
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        cin >> elenconomi[i];
+    }
+
+    cin >> nome;
+
+    if (Ricerca(elenconomi, nome, posizione)) {
+        cout << posizione;
+    } else {
+        cout << "non presente";
+    }
+
+    return 0;
 }
